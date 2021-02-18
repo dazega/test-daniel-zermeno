72fe1dc3242053d085099a89fd58eb4370321b8d
Daniel Zermeño <dazega97@hotmail.com>
2021-02-17 22:47:02 -0600

    Add of controller for update a document

--- controllers/document.js
+++ controllers/document.js
@@ -1,4 +1,4 @@
-import { Document } from "../models";
+import { Document, RecordHistory } from "../models";
 
 export const createDocument = async (req, res) => {
   const {
@@ -23,4 +23,49 @@ export const createDocument = async (req, res) => {
   });
 
   return res.status(200).json({ document }); 
+}
+
+export const updateDocument = async (req, res) => {
+  let {
+    documentId
+  } = req.params;
+
+  const {
+    content = '',
+    name = '',
+    category = ''
+  } = req.body;
+
+  const {
+    id: userId
+  } = req.decoded;
+
+  documentId = parseInt(documentId);
+  if (isNaN(documentId) || content === '' || name === '' || category === '') return res.status(400).json({ message: 'wrong parameters' });
+
+  
+  const document = await Document.findOne({
+    where: {
+      id: documentId
+    }
+  });
+  
+  if(!document) return res.status(404).json({ message: 'Document not found' });
+  
+  if(document.userId !== userId) return res.status(403).json({ message: 'You are not the owner of the document' });
+
+  await RecordHistory.create({
+    category: document.category,
+    content: document.content,
+    documentId: document.id,
+    name: document.name  
+  });
+  
+  await document.update({
+    content,
+    name,
+    category
+  });
+
+  return res.status(200).json({ document });
 }
--- routes/document.js
+++ routes/document.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { createDocument } from "../controllers/document";
+import { createDocument, updateDocument } from "../controllers/document";
 import { checkToken } from "../middleware/middleware";
 
 const router = express.Router();
 
 router.post('/create', checkToken, createDocument);
+router.put('/:documentId', checkToken, updateDocument);
 
 export default router;
